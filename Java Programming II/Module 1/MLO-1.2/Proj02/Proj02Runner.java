dbcaf0f51c07b92dbff186ea9e773fab4aec9a66
Whit Allee <whitallee@gmail.com>
2026-02-11 16:14:55 -0600

    Completed Proj02 for Java course diff --git c/Java Programming II/Module 1/MLO-1.2/Proj02/AlleeProj02.zip i/Java Programming II/Module 1/MLO-1.2/Proj02/AlleeProj02.zip new file mode 100644 index 0000000..d947b67 Binary files /dev/null and i/Java Programming II/Module 1/MLO-1.2/Proj02/AlleeProj02.zip differ diff --git c/Java Programming II/Module 1/MLO-1.2/Proj02/Compare01.png i/Java Programming II/Module 1/MLO-1.2/Proj02/Compare01.png new file mode 100644 index 0000000..7fc9705 Binary files /dev/null and i/Java Programming II/Module 1/MLO-1.2/Proj02/Compare01.png differ diff --git c/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02.class i/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02.class new file mode 100755 index 0000000..310348f Binary files /dev/null and i/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02.class differ diff --git c/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02Runner.class i/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02Runner.class new file mode 100755 index 0000000..01742dc Binary files /dev/null and i/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02Runner.class differ diff --git c/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02Runner.java i/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02Runner.java index d6c4d3d..57c4b4c 100644 --- c/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02Runner.java +++ i/Java Programming II/Module 1/MLO-1.2/Proj02/Proj02Runner.java @@ -1,13 +1,15 @@  /* File Proj02Runner -This skeleton code will compile but it won't run -because there is some missing code. +Whit Allee +2/11/2026 +ITSE-2317-001 +Professor Baldwin  *****************************************************/  import java.util.TreeSet;  import java.util.ArrayList;  import java.util.Collection;  import java.util.Iterator;
    -class Proj02Runner{
    +class Proj02Runner implements Comparable{
    
       int data = 0;
    
    @@ -18,7 +20,7 @@ class Proj02Runner{
           "I certify that this program is my own work \n"+
           "and is not the work of others. I agree not \n" +
           "to share my solution with others.\n" +
    -      "Replace this line with your name\n");
    +      "Whit Allee\n");
       }//end overloaded constructor
    
       Proj02Runner(int data){//overloaded constructor
    @@ -27,5 +29,27 @@ class Proj02Runner{
         this.data = data;
       }//end overloaded constructor
    
    +  public String toString(){
    +    return "" + data;
    +  }//end toString()
    +
    +  public int compareTo(Object o){
    +    if(!(o instanceof Proj02Runner))
    +      throw new ClassCastException();
    +    if(((Proj02Runner)o).data < data)
    +      return 1;
    +    if(((Proj02Runner)o).data > data)
    +      return -1;
    +    else return 0;
    +  }//end compareTo()
    +
    +  public boolean equals(Object o){
    +    if(!(o instanceof Proj02Runner))
    +      return false;
    +    if(((Proj02Runner)o).data == data)
    +      return true;
    +    else return false;
    +  }//end equals()
    +
     }//end class Proj02Runner

--- Java Programming II/Module 1/MLO-1.2/Proj02/Proj02Runner.java
+++ Java Programming II/Module 1/MLO-1.2/Proj02/Proj02Runner.java
@@ -1,13 +1,15 @@
 /* File Proj02Runner
-This skeleton code will compile but it won't run
-because there is some missing code.
+Whit Allee
+2/11/2026
+ITSE-2317-001
+Professor Baldwin
 *****************************************************/
 import java.util.TreeSet;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
-class Proj02Runner{
+class Proj02Runner implements Comparable{
 
   int data = 0;
 
@@ -18,7 +20,7 @@ class Proj02Runner{
       "I certify that this program is my own work \n"+
       "and is not the work of others. I agree not \n" +
       "to share my solution with others.\n" +
-      "Replace this line with your name\n");
+      "Whit Allee\n");
   }//end overloaded constructor
   
   Proj02Runner(int data){//overloaded constructor
@@ -26,6 +28,28 @@ class Proj02Runner{
     // the incoming data value
     this.data = data;
   }//end overloaded constructor
+
+  public String toString(){
+    return "" + data;
+  }//end toString()
+
+  public int compareTo(Object o){
+    if(!(o instanceof Proj02Runner))
+      throw new ClassCastException();
+    if(((Proj02Runner)o).data < data)
+      return 1;
+    if(((Proj02Runner)o).data > data)
+      return -1;
+    else return 0;
+  }//end compareTo()
+
+  public boolean equals(Object o){
+    if(!(o instanceof Proj02Runner))
+      return false;
+    if(((Proj02Runner)o).data == data)
+      return true;
+    else return false;
+  }//end equals()
 
 }//end class Proj02Runner
 
